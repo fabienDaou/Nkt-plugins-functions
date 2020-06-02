8f5ec47a81cd28e234a2daa9bbd9dc796385cc4f
Hycariss <fabien.daoulas@gmail.com>
2020-06-02 23:11:19 +0100

    Add OAuth github function.

---
+++ githuboauth/index.js
@@ -0,0 +1,21 @@
+const got = require("got");
+const { log } = require("../shared/logger");
+
+module.exports = async function (context, req) {
+    const { code } = req.query;
+    if (code) {
+        const clientId = process.env.GithubNktClientId;
+        const clientSecret = process.env.GithubNktClientSecret;
+        const url = `https://github.com/login/oauth/access_token?client_id=${clientId}&client_secret=${clientSecret}&code=${code}`
+        const { body } = await got.get(url, { headers: { "Accept": "application/json" } });
+        context.res = {
+            status: 200,
+            body: body
+        };
+    } else {
+        context.res = {
+            status: 400,
+            body: "Please provide the code from Guthub."
+        };
+    }
+};
